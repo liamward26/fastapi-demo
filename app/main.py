d2b45edb3c1a850a4e24f12026583494d82a25f6
liamward26 <vhk7vr@virginia.edu>
2024-11-13 13:53:32 -0500

    Add DP1 work

--- app/main.py
+++ app/main.py
@@ -5,31 +5,53 @@ from typing import Optional
 from pydantic import BaseModel
 import json
 import os
+import mysql.connector
+from mysql.connector import Error
+
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DBPASS')
+DB = "vhk7vr"
+
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, 
+database=DB)
+cur=db.cursor()
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")  # zone apex
-def zone_apex():
-    return {"Hello": "Hello API We here now"}
-
-@app.get("/add/{a}/{b}")
-def add(a: int, b: int):
-    return {"sum": a + b}
-
-@app.get("/multiply/{c}/{d}")
-def multiply(c: int, d: int):
-	return {"product": c * d}
-
-@app.get("/square/{e}")
-def square(e: int):
-    return {"square": e * e}
-
-@app.get("/percent/{f}")
-def percent(f: int):
-    return {"percent": f * 0.01}
-
-@app.get("/subtract/{g}/{h}")
-def subtract(g: int, b: int):
-    return {"difference": g - h}
-
-
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+    
+@app.get('/songs')
+def get_songs():
+    query = "SELECT songs.title, songs.album, songs.artist, songs.year, 
+songs.file, songs.image, genres.genre FROM songs JOIN genres ON 
+songs.genre=genres.genreid ORDER BY songs.title ASC;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
